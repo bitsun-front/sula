73d76c9f5425982ac33b00ddab8275efd982855c
赵杰诚 <jiecheng.zjc@bitsun-inc.com>
2022-04-27 21:07:01 +0800

    style: bs-searchSelect

--- packages/sula/src/field-plugin/bsFieldPlugin/bsSearchSelect.tsx
+++ packages/sula/src/field-plugin/bsFieldPlugin/bsSearchSelect.tsx
@@ -263,6 +263,7 @@ export default class BitSunSearchSelect extends React.Component<any> {
         filterOption={(input, option) => {
           return option.children && option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0;
         }}
+        getPopupContainer={triggerNode => triggerNode.parentElement}
         {...restProps}
       >
         {source.map((item) => {
